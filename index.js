b5503669545deab765b1357184ffb2f2154c2262
pittman.anthony17@gmail.com <pittman.anthony17@gmail.com>
2023-09-29 18:52:16 -0700

    changes

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(img, left, bottom){
+    let image = document.createElement('img')
+    image.src = img
+    image.style.position = 'fixed'
+    image.style.left = left + 'px'
+    image.style.bottom = bottom + 'px'
+    document.body.append(image)
+    return image
+}
+
+
+function newItem(img, left, bottom){
+    let image = newImage(img, left, bottom)
+
+    image.addEventListener('dblclick', () => {
+        image.remove()
+    })
+
+}
+
+
+
+
+ 
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
